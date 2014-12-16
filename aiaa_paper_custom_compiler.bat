bbaa1f9d21cfd9906b257da9db92b5827de74d11
mfozcan <metin.ozcan@gatech.edu>
2014-12-16 07:22:20 -0500

    JPC 2015 TTECTrA - Extended Abstract - First Draft

--- aiaa_paper_custom_compiler.bat
+++ aiaa_paper_custom_compiler.bat
@@ -1,9 +1,10 @@
-REM pdflatex AIAA_template.tex
-xelatex AIAA_template.tex
-bibtex AIAA_template
-makeindex AIAA_template.nlo -s nomencl.ist -o AIAA_template.nls
-REM pdflatex AIAA_template.tex
-xelatex AIAA_template.tex
-REM pdflatex AIAA_template.tex
-xelatex AIAA_template.tex
-REM start Comparing_Input_Signals_Using_Density_Estimation.pdf
+REM pdflatex JPC_TTECTrA_2015.TEX
+xelatex JPC_TTECTrA_2015.TEX
+bibtex JPC_TTECTrA_2015
+makeindex JPC_TTECTrA_2015.nlo -s nomencl.ist -o JPC_TTECTrA_2015.nls
+REM pdflatex JPC_TTECTrA_2015.TEX
+xelatex JPC_TTECTrA_2015.TEX
+REM pdflatex JPC_TTECTrA_2015.TEX
+xelatex JPC_TTECTrA_2015.TEX
+REM start JPC_TTECTrA_2015.TEX.pdf
+cls
